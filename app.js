210bfd78c9a400caaa5ceb69e487f0f637cf719b
Jenniferrmuniz <Jennifer.r.muniz@gmail.com>
2019-11-05 16:19:53 -0500

    cleaned up

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const allApps = require('./seed.js');
 require('dotenv').config();
 
 
-
 // Get range parameters
 function buildRange(req) {
   return {
@@ -17,47 +16,32 @@ function buildRange(req) {
 }
 
 
-
 // Sort apps by ID
 function sortByID(apps, order) {
   apps.sort((a, b) => {
-    if (order == 'asc') {
-      return a.id - b.id;
-    }
-    else {
-      return b.id - a.id;
-    }
+    return (order=='asc') ? a.id-b.id : b.id-a.id;
   })
   return apps;
 }
-
 
 
 // Sort apps by name
 function sortByName(apps, order) {
   apps.sort((a, b) => {
-    if (order == 'asc') {
-      if (a.name < b.name) {
-        return -1;
-      }
-      if (a.name > b.name) {
-        return 1;
-      }
-      return 0;
-    }
-    else {
-      if (a.name > b.name) {
-        return -1;
-      }
-      if (a.name < b.name) {
-        return 1;
-      }
-      return 0;
+    switch (order) {
+      case 'asc':
+        if (a.name < b.name) return -1; 
+        if (a.name > b.name) return 1; 
+        return 0;
+
+      case 'desc':
+        if (a.name > b.name) return -1; 
+        if (a.name < b.name) return 1; 
+        return 0;
     }
   })
   return apps;
 }
-
 
 
 // Finds object in array to get start/end points
@@ -65,38 +49,25 @@ function findObj(sortedList, boundary) {
   result = sortedList.find(obj => {
     return obj.id === Number(boundary) || obj.name === boundary;
   })
-  if (sortedList.indexOf(result) < 0) {
-    return undefined;
-  }
+  if (sortedList.indexOf(result) < 0) return undefined; 
   return sortedList.indexOf(result);
 }
 
 
-
 // Set endpoint to last app on page
 function getEndpoint(start, end, max) {
-  if (end < start + max && end > start) {
-    return end;
-  }
-  else {
-    return start + max;
-  }
+  return (end<start+max && end>start) ? end : start+max;
 }
-
 
 
 // Paginate and return requested apps in order
 function paginateAndSort(apps, req) {
-  
+
   let sorted;
   const incomingRange = buildRange(req);
 
-  if(incomingRange.by == 'name'){
-    sorted = sortByName(apps, incomingRange.order);
-  }
-  else if(incomingRange.by == 'id'){
-    sorted = sortByID(apps, incomingRange.order);
-  }
+  if (incomingRange.by == 'name') sorted = sortByName(apps, incomingRange.order);
+  else if (incomingRange.by == 'id') sorted = sortByID(apps, incomingRange.order);
 
   let range = {
     start: findObj(sorted, incomingRange.start) || 0,
@@ -112,14 +83,13 @@ function paginateAndSort(apps, req) {
 
 
 // Route that does not have params or endpoint
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
   res.send('MDLive Pagination challenge! ( type params in url to paginate api - ex: https://pagination-challenge.herokuapp.com/apps/range?by=id&start=2 ) ');
 })
 
 
-
 // Endpoint but no range params are specified so it is set to default case
-app.get('/apps', (req, res) =>{  
+app.get('/apps', (req, res) => {
   req.query.by = 'id'
   res.json(paginateAndSort(allApps, req));
 })
